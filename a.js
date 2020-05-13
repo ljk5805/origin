d05b814c23a11203f4cb2729c8b94fdaca7ce404
ljk5805 <2283845241@qq.com>
2020-05-13 11:19:27 +0800

    two commit

--- a.js
+++ a.js
@@ -1,1 +1,7 @@
-我是你那吧不吧不吧不不不不不吧吧不吧不吧不吧吧不不不不吧吧
+git init 建立git文件
+git remote add 本地仓库名字 URL  建立连接
+git remote -v 查看连接
+git checkout -b jianke 建立新的分支
+git add . 提交到暂存区
+git commit -m "信息" 转移
+git push -u URL 提交
